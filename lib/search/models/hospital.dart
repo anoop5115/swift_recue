22aa93976502af932ba05117d58b1aa2eb9f8611
anoop5115 <anooppkr2003@gmail.com>
2024-04-17 17:33:14 +0530

    latest

--- lib/search/models/hospital.dart
+++ lib/search/models/hospital.dart
@@ -7,11 +7,49 @@ class hosp {
   hosp({this.city, this.name, this.contact, this.color});
 
   // List of hospital objects
-  List<hosp> hosplist = [
-    hosp(
-        city: "kollam",
-        name: "anoop",
-        contact: 9746195665,
-        color: true), // Using named arguments
-  ];
 }
+
+List<hosp> hosp_list = [
+  hosp(
+      city: "New York", name: "NYC Hospital", contact: 1234567890, color: true),
+  hosp(
+      city: "Los Angeles",
+      name: "LA Medical Center",
+      contact: 9876543210,
+      color: false),
+  hosp(
+      city: "Chicago",
+      name: "Windy City Clinic",
+      contact: 2468109753,
+      color: true),
+  hosp(
+      city: "Houston",
+      name: "Houston General",
+      contact: 1357924680,
+      color: false),
+  hosp(
+      city: "Miami",
+      name: "Miami Health Center",
+      contact: 3692581470,
+      color: false),
+  hosp(
+      city: "San Francisco",
+      name: "SF Medical Institute",
+      contact: 9870123456,
+      color: false),
+  hosp(
+      city: "Dallas",
+      name: "Dallas Medical Clinic",
+      contact: 4567890123,
+      color: false),
+  hosp(
+      city: "Atlanta",
+      name: "Atlanta Regional Hospital",
+      contact: 6543210987,
+      color: false),
+  hosp(
+      city: "Seattle",
+      name: "Seattle Community Clinic",
+      contact: 7890123456,
+      color: true),
+];
--- lib/search/views/search_page.dart
+++ lib/search/views/search_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/container.dart';
+import 'package:flutter_application_1/search/models/hospital.dart';
 import 'package:flutter_application_1/signin/components/textfield.dart';
+import 'package:flutter_application_1/video_call/video_call_page.dart';
 import 'package:get/get.dart';
 import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
 
@@ -43,22 +45,111 @@ class SearchPage extends StatelessWidget {
               ),
               SliverList(
                   delegate: SliverChildBuilderDelegate(
-                      childCount: 5,
+                      childCount: hosp_list.length,
                       (context, index) => Padding(
                             padding: EdgeInsets.only(top: 5, bottom: 5),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                    begin: Alignment.topLeft,
-                                    end: Alignment.bottomRight,
-                                    colors: [
-                                      Colors.blue,
-                                      Colors.grey.withOpacity(.4),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    boxShadow: [
+                                      BoxShadow(
+                                          color: Color.fromARGB(255, 21, 24, 21)
+                                              .withOpacity(.3),
+                                          spreadRadius: 4,
+                                          blurRadius: 3)
                                     ],
+                                    gradient: LinearGradient(
+                                      begin: Alignment.topLeft,
+                                      end: Alignment.bottomRight,
+                                      colors: [
+                                        Colors.blue,
+                                        Colors.grey.withOpacity(.4),
+                                      ],
+                                    ),
+                                    borderRadius: BorderRadius.circular(20)),
+                                width: double.infinity,
+                                height: 150,
+                                child: Center(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 20, right: 15),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceAround,
+                                          children: [
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [
+                                                Text(
+                                                  hosp_list[index]
+                                                      .name
+                                                      .toString()
+                                                      .toUpperCase(),
+                                                  style: TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                )
+                                              ],
+                                            ),
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [
+                                                Text(
+                                                    "city:${hosp_list[index].city}")
+                                              ],
+                                            ),
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [
+                                                Text("availablity status "),
+                                                CircleAvatar(
+                                                  backgroundColor:
+                                                      hosp_list[index].color ==
+                                                              true
+                                                          ? Colors.green
+                                                          : Colors.red,
+                                                  radius: 5,
+                                                )
+                                              ],
+                                            ),
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceAround,
+                                              children: [
+                                                Text(
+                                                    "+91-${hosp_list[index].contact.toString()}")
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                        Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            InkWell(
+                                                onTap: () {
+                                                  Get.to(VideoCallPage(
+                                                    callID: hosp_list[index]
+                                                        .contact
+                                                        .toString(),
+                                                  ));
+                                                },
+                                                child: Icon(Icons.video_call))
+                                          ],
+                                        )
+                                      ],
+                                    ),
                                   ),
-                                  borderRadius: BorderRadius.circular(20)),
-                              width: double.infinity,
-                              height: 150,
+                                ),
+                              ),
                             ),
                           )))
             ],
